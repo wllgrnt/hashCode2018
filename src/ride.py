34c1fdf45febf783a22fdc04209fca7a0ea2dde5
Will Grant <wpg23@cam.ac.uk>
2018-03-01 19:07:15 +0000

    terrible repr for rides

--- src/ride.py
+++ src/ride.py
@@ -1,8 +1,15 @@
 class Ride:
-
     def __init__(self, ride_id, origin, destination, start_time, finish_time):
         self.ride_id = ride_id
         self.origin = origin
         self.destination = destination
         self.start_time = start_time
         self.finish_time = finish_time
+
+    def __repr__(self):
+        return f""""(ride_id: {self.ride_id},
+    origin: {self.origin},
+    destination: {self.destination},
+    start time: {self.start_time},
+    finish time: {self.finish_time}
+)\n"""
