3c21f308c90018608030d4fbc345172e3ca71f69
ml-evs <me388@cam.ac.uk>
2018-03-01 18:54:52 +0000

    Init for ride

---
+++ src/ride.py
@@ -0,0 +1,7 @@
+class Ride:
+
+    def __init__(self, start_time, finish_time, origin, destination):
+        self.start_time = start_time
+        self.finish_time = finish_time
+        self.origin = origin
+        self.destination = destination
