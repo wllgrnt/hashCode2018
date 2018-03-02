7fa34118f714f477f9770f68a7f018e22c7623e3
me388 <matthew.evans1992@gmail.com>
2018-03-02 01:15:36 +0000

    48 million src

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import argparse
 from input import processInputFile
 from output import writeSolution
 from grid import getDistance
+from copy import deepcopy
 from ascii_graph import Pyasciigraph
 import tqdm
 
@@ -16,51 +17,49 @@ total_num_rides = len(rides)
 ride_bar = tqdm.tqdm(total=len(rides))
 
 while len(rides) > 0:
-    # for ind, car in tqdm.tqdm(enumerate(fleet), total=len(fleet)):
     assigned = False
     for ind, car in enumerate(fleet):
         time = int(car.time)
         if car.time < all_time:
-            # print('Time for vehicle {} = {}/{}'.format(ind, time, all_time))
             best_score_per_time = None
-            quickest_finish_time = None
-            for ride in rides:
+            best_ride = None
+            for ride_ind, ride in enumerate(rides):
                 trial_dist = int(getDistance(car.current_position, ride.origin))
                 wait_time = max(int(ride.start_time) - (time + trial_dist), 0)
-                projected_finish_time = wait_time + trial_dist + ride.length
-                score = ride.length + bonus
-                if trial_dist <= int(ride.start_time) - int(time):
-                    score += bonus
-                # score_per_time = score / (projected_finish_time - time)
-                # if ride finishes sooner than all others
-                if quickest_finish_time is None or projected_finish_time < quickest_finish_time:
-                # if best_score_per_time is None or score_per_time > best_score_per_time:
-                    # if ride finishes in time allowed
-                    if projected_finish_time <= int(ride.finish_time):
+                time_to_complete = trial_dist + wait_time + ride.length
+                projected_finish_time = time_to_complete + time 
+                if projected_finish_time < int(ride.finish_time):
+                    score = ride.length
+                    if trial_dist <= int(ride.start_time) - int(time):
+                        score += bonus
+                    score_per_time = score / time_to_complete
+                    if best_score_per_time is None or score_per_time > best_score_per_time:
                         dist = trial_dist
                         best_ride = ride
-                        quickest_finish_time = projected_finish_time
-                        # best_score_per_time = score_per_time
-            if quickest_finish_time is None:
+                        best_ride_ind = ride_ind
+                        best_wait_time = wait_time
+                        best_score_per_time = score_per_time
+                        best_ride.projected_finish_time = projected_finish_time
+                        best_ride.time_to_complete = time_to_complete
+            if best_ride is None:
                 time = all_time + 1
             else:
                 assigned = True
-                car.time += best_ride.length + dist + wait_time
-                time += car.time
-                car.assign_ride(best_ride)
-                ride_index = rides.index(best_ride)
-                rides.pop(ride_index)
-                ride_bar.update(1)
+                if best_ride.projected_finish_time < all_time:
+                    car.time += best_ride.time_to_complete
+                    car.current_position = best_ride.destination
+                    car.assign_ride(best_ride)
+                    ride_index = best_ride_ind
+                    rides.pop(ride_index)
+                    ride_bar.update(1)
     if ind == len(fleet) - 1 and not assigned:
         break
-            # print('Remaining rides {}'.format(len(rides)))
 
 ride_bar.close()
 
 rides_fulfilled = 0
 hist_dict = {i: 0 for i in range(5)}
 for ind, car in enumerate(fleet):
-    # print('{} {}'.format(ind, car.num_rides))
     if car.num_rides not in hist_dict:
         hist_dict[car.num_rides] = 0
     hist_dict[car.num_rides] += 1
